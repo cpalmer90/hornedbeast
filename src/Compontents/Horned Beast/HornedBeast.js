aef53c909d24a83474b02f25f2847dedb2e0b2f5
Chris Palmer <christopherpalmer283@gmail.com>
2023-07-19 14:37:19 +0100

    updated horned beasts mapping data

--- src/Compontents/Horned Beast/HornedBeast.js
+++ src/Compontents/Horned Beast/HornedBeast.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 import "./HornedBeast.css";
 
-export default function HornedBeast({ title, imgUrl, description }) {
+export default function HornedBeast({
+  title,
+  imgUrl,
+  description,
+  handleModal,
+  beastObject,
+}) {
   const [fav, setfav] = useState(0);
 
   function handleFav() {
@@ -11,7 +17,7 @@ export default function HornedBeast({ title, imgUrl, description }) {
   return (
     <div className="hb-container">
       <h2>{title}</h2>
-      <img src={imgUrl} alt={title} />
+      <img src={imgUrl} alt={title} onClick={() => handleModal(beastObject)} />
       <p>{description}</p>
       <p>
         <span className="heart" onClick={handleFav}>
